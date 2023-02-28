e6cb69e1153daa6c8fea8c8a101e2277b887ca75
João Maria Henriques Madeira Pereira <joaompereira07@gmail.com>
2023-02-28 10:37:26 +0000

    project structure

---
+++ HDL/Server/src/main/java/sec/G31/App.java
@@ -0,0 +1,13 @@
+package sec.G31;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
